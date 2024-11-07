67392858fc69a8f5761c5c5b4a492d51eac0bab5
quandhd94 <quandhd94@gmail.com>
2024-11-07 12:27:49 +0700

    Update code

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import NavBar from "./components/NavBar";
 import Footer from "./components/Footer";
 import HomePage from "./pages/HomePage/HomePage";
@@ -16,10 +16,8 @@ import LiveChatPage from "./pages/LiveChatPage";
 function App() {
   return (
     <UserProvider>
-      {" "}
       {/* Wrap with UserProvider */}
       <ProductProvider>
-        {" "}
         {/* Wrap your app with ProductProvider */}
         <Router>
           <div className="App">
